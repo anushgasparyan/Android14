bb4516d95cb3ac6d41e8b3fa576c57ec7d907297
overlord9600 <vahanmur007@gmail.com>
2020-11-10 15:32:20 +0400

    commit

--- app/src/main/java/com/example/android14/MainActivity.java
+++ app/src/main/java/com/example/android14/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.android14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("my", "hello");
     }
 }
