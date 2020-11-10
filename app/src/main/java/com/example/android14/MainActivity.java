e2a1279a65f0b46a235a5774644b04d57e3791ea
overlord9600 <vahanmur007@gmail.com>
2020-11-10 15:34:40 +0400

    commit

--- app/src/main/java/com/example/android14/MainActivity.java
+++ app/src/main/java/com/example/android14/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Button b;
+        Toast.makeText(this, "hello", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
 
     }
